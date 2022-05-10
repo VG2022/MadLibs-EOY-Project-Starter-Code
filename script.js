ec743431df3baaf5586558340acfc355afa24561
VG2022 <104461125+VG2022@users.noreply.github.com>
2022-05-10 12:20:16 -0700

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let storiesCreated;
-$(".submit").click (function(){
+let storiesCreated = 0;
+$(".submit").click(function() {
+    storiesCreated = storiesCreated + 1;
+    $(".count").text(storiesCreated);
+
+
     let name = $(".name-input").val();
-     let transport = $(".transport-input").val(); 
-     let adjective = $(".adjective-input").val(); 
-     let time = $(".number-input").val(); 
-    
-    $(".story").append ("<p>" + " One day my friend " + name + " and I decided to take a trip to the beach, so we packed our bags and took the " + transport + " over. It was a " + adjective + " day at the beach! We ended up staying for " + time + " hours!" + "</p>");
+    let transport = $(".transport-input").val();
+    let adjective = $(".adjective-input").val();
+    let time = $(".number-input").val();
+
+    $(".story").append("<p>" + " One day my friend " + name + " and I decided to take a trip to the beach, so we packed our bags and took the " + transport + " over. It was a " + adjective + " day at the beach! We ended up staying for " + time + " hours!" + "</p>");
 });
